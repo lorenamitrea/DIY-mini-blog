f345d3f122b50648a4a4ecbb16b987c73c10aa03
Lorena Mitrea <mitrea.lorenageo@gmail.com>
2019-12-20 16:03:40 +0200

    modified urls for redirect to todo page

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=500)
+
+
+class Task(models.Model):
+
+    name = models.CharField(max_length=1000)
+    status = models.BooleanField(default=False)
+    board = models.ForeignKey(Board, on_delete=models.SET_NULL, null=True)
+    details = models.CharField(max_length=1000, null=True, blank=True)
