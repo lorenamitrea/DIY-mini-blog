c5cc49ac6de8630bdd9a8f3b995d7b822e1561d8
Lorena Mitrea <mitrea.lorenageo@gmail.com>
2020-01-13 16:44:13 +0200

    remove super parameters

--- todolist/forms.py
+++ todolist/forms.py
@@ -18,7 +18,7 @@ class BoardShare(forms.Form):
 
     def __init__(self, *args, **kwargs):
         self.current_user = kwargs.pop('current_user')
-        super(BoardShare, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         users_friends = Friend.objects.filter(user_id=self.current_user)
         friends = [(entry.friend.id, entry.friend.username) for entry in users_friends]
         self.fields['friend_choice'] = forms.ChoiceField(choices=friends, label='Share with')
@@ -32,7 +32,7 @@ class UserCreationFormExtended(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ('email',)
 
     def save(self, commit=True):
-        user = super(UserCreationFormExtended, self).save()
+        user = super().save()
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
